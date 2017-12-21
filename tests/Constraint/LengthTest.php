bf84bea2d4705872f4840180a655697ffe3f8267
Dan Burzynski <contax@phrenetic.org>
2017-12-21 22:27:00 +0000

    Length test

---
+++ tests/Constraint/LengthTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace C0ntax\ParsleyBundle\Tests\Constraint;
+
+use C0ntaX\ParsleyBundle\Constraint\Length;
+
+class LengthTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGetConstraintId()
+    {
+        self::assertEquals('length', Length::getConstraintId());
+    }
+
+    public function testGetViewAttr()
+    {
+        $pattern = new Length(1, 20);
+        self::assertEquals(['data-parsley-length' => '[1, 20]'], $pattern->getViewAttr());
+
+        $pattern = new Length(1, 20, 'Error message');
+        self::assertEquals(
+            ['data-parsley-length' => '[1, 20]', 'data-parsley-length-message' => 'Error message'],
+            $pattern->getViewAttr()
+        );
+    }
+}
